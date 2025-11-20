c58d14bf3549f12aa3dba970f2115faa476bf8f5
mts_dev4 <mts_dev4@stephensdirect.com>
2025-11-20 09:15:27 -0600

    Feat: Initial Auth

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { useAuthStore } from './stores/authStore'
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +8,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const authStore = useAuthStore()
+authStore.initializeAuth()
 
 app.mount('#app')
